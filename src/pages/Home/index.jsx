a28aa4fce8a4062139428cd651314ec93477ff7c
CARLOS HENRIQUE CORREA RODRIGUES <carlos.rodrigues3@alunos.sc.senac.br>
2023-07-11 20:46:26 -0300

    feat: add initial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import styles from './Home.module.css';
+import React from "react";
+import styles from "./Home.module.css";
+import Navbar from "../Navbar";
 
-const Home = () =>{
-    return (
-        <div className={styles.home}>
-            <h1>Home</h1>
-        </div>
-    )
-}
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <Navbar />
+      <h1>Home</h1>
+    </div>
+  );
+};
 
 export default Home;
