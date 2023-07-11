e0b10309f4c29848ab43f040919d5445384eafea
MakenRosa <maken.cristhian@gmail.com>
2023-07-11 20:41:01 -0300

    feat: routing index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home';
+import ESG from './pages/ESG';
+import Reports from './pages/Reports';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  [
+    { path: '/', element: <Home /> },
+    { path: '/esg', element: <ESG /> },
+    { path: '/reports', element: <Reports /> },
+  ]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
