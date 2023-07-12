b821b4642c445a33cf6c0b11cb34b33ddbf2568a
= <=>
2023-07-12 19:57:42 -0300

    feat: (mvv)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,67 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card"
+import { Container, Card } from "react-bootstrap";
 import CarouselSite from "components/CarouselSite";
+import CardGroup from "react-bootstrap/CardGroup";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <CarouselSite />
       <Container>
-        <Card>
-          <Card.Body>
-            <Card.Title>Little Green</Card.Title>
-            <Card.Text>
-              Little Green é uma empresa de consultoria em sustentabilidade que
-              tem como objetivo auxiliar empresas a se tornarem mais
-              sustentáveis.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        <CardGroup className={styles.cardGroup}>
+          <Card className={styles.card}>
+            <Card.Body>
+              <Card.Title className={styles.cardTitle}>Missão</Card.Title>
+              <Card.Text className={styles.cardText}>
+                Nossa missão é proporcionar uma educação de excelência,
+                preparando nossos alunos para se tornarem líderes responsáveis e
+                comprometidos com o desenvolvimento sustentável, a igualdade
+                social e a ética nos negócios.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className={styles.card}>
+            <Card.Body>
+              <Card.Title className={styles.cardTitle}>Visão</Card.Title>
+              <Card.Text className={styles.cardText}>
+                Buscamos ser uma referência em práticas educacionais que
+                integram os princípios de ESG. Queremos ser reconhecidos como
+                uma instituição que forma profissionais capacitados e
+                conscientes de seu papel na construção de um mundo mais justo,
+                equilibrado e sustentável.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className={styles.card}>
+            <Card.Body>
+              <Card.Title className={styles.cardTitle}>Valores</Card.Title>
+              <Card.Text className={styles.cardText}>
+                <ul>
+                  <li>
+                    <strong>Sustentabilidade:</strong> Práticas sustentáveis,
+                    redução do impacto ambiental e conservação de recursos
+                    naturais.
+                  </li>
+                  <li>
+                    <strong>Diversidade e Inclusão:</strong> Valorização da
+                    diversidade, ambiente inclusivo, respeito e
+                    representatividade.
+                  </li>
+                  <li>
+                    <strong>Responsabilidade Social:</strong> Contribuição
+                    positiva para a sociedade, envolvimento em iniciativas
+                    comunitárias e impacto social positivo.
+                  </li>
+                  <li>
+                    <strong>Ética e Transparência:</strong> Padrões éticos
+                    elevados, transparência em práticas e comunicações.
+                  </li>
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
       </Container>
     </div>
   );
