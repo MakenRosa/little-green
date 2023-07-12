84bd1f22ce59611b9347209c51fb5176d934ebc5
MakenRosa <maken.rosa@nexti.com>
2023-07-12 15:48:43 -0300

    feat: add card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import CarouselSite from "../../components/CarouselSite";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card"
+import CarouselSite from "components/CarouselSite";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <CarouselSite />
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title>Little Green</Card.Title>
+            <Card.Text>
+              Little Green é uma empresa de consultoria em sustentabilidade que
+              tem como objetivo auxiliar empresas a se tornarem mais
+              sustentáveis.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 };
