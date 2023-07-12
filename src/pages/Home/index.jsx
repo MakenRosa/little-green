acfb1596a22e900414db5a52f70323807c8485af
MakenRosa <maken.rosa@nexti.com>
2023-07-12 13:19:25 -0300

    style: updated sass file import

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./Home.module.css";
+import styles from "./Home.module.scss";
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
+      <h1 className={styles.home__title}>home</h1>
     </div>
-  );
+);
 };
 
 export default Home;
