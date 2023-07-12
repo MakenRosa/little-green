b4e0d53f3bce44b2a9676be6073e7d9b263129c1
= <=>
2023-07-12 20:01:39 -0300

    fixed (containerMVV)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,68 +1,13 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import { Container, Card } from "react-bootstrap";
 import CarouselSite from "components/CarouselSite";
-import CardGroup from "react-bootstrap/CardGroup";
+import ContainerMVV from "components/ContainerMVV/index";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <CarouselSite />
-      <Container>
-        <CardGroup className={styles.cardGroup}>
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>Missão</Card.Title>
-              <Card.Text className={styles.cardText}>
-                Nossa missão é proporcionar uma educação de excelência,
-                preparando nossos alunos para se tornarem líderes responsáveis e
-                comprometidos com o desenvolvimento sustentável, a igualdade
-                social e a ética nos negócios.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>Visão</Card.Title>
-              <Card.Text className={styles.cardText}>
-                Buscamos ser uma referência em práticas educacionais que
-                integram os princípios de ESG. Queremos ser reconhecidos como
-                uma instituição que forma profissionais capacitados e
-                conscientes de seu papel na construção de um mundo mais justo,
-                equilibrado e sustentável.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title className={styles.cardTitle}>Valores</Card.Title>
-              <Card.Text className={styles.cardText}>
-                <ul>
-                  <li>
-                    <strong>Sustentabilidade:</strong> Práticas sustentáveis,
-                    redução do impacto ambiental e conservação de recursos
-                    naturais.
-                  </li>
-                  <li>
-                    <strong>Diversidade e Inclusão:</strong> Valorização da
-                    diversidade, ambiente inclusivo, respeito e
-                    representatividade.
-                  </li>
-                  <li>
-                    <strong>Responsabilidade Social:</strong> Contribuição
-                    positiva para a sociedade, envolvimento em iniciativas
-                    comunitárias e impacto social positivo.
-                  </li>
-                  <li>
-                    <strong>Ética e Transparência:</strong> Padrões éticos
-                    elevados, transparência em práticas e comunicações.
-                  </li>
-                </ul>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </CardGroup>
-      </Container>
+      <ContainerMVV />
     </div>
   );
 };
