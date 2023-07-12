520a7f00a41a221ebfce03bb82f8c8c1306a6f63
MakenRosa <maken.cristhian@gmail.com>
2023-07-12 20:47:48 -0300

    feat: added reports section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Home.module.scss";
 import CarouselSite from "components/CarouselSite";
-import ContainerMVV from "components/ContainerMVV/index";
+import ContainerMVV from "components/ContainerMVV";
+import SustainabilityReports from "components/SustainabilityReports/index";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <CarouselSite />
       <ContainerMVV />
+      <SustainabilityReports />
     </div>
   );
 };
