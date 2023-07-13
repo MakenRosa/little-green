894f08d22788eccea106bb8b85b79b639ff93914
MakenRosa <maken.rosa@nexti.com>
2023-07-13 15:15:45 -0300

    fix: fixed navbar link scroll

--- src/components/ContainerMVV/index.jsx
+++ src/components/ContainerMVV/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./ContainerMVV.module.css";
+import styles from "./ContainerMVV.module.scss";
 import { Container, Card } from "react-bootstrap";
 import CardGroup from "react-bootstrap/CardGroup";
 
